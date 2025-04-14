cbde90e4cdf50deab3b54557f84b5df4b71286a4
Rachel <hurui.lcy@gmail.com>
2025-04-13 21:43:06 -0600

    update fetch weather api

--- script.js
+++ script.js
@@ -13,6 +13,27 @@ const swiper = new Swiper('.swiper', {
   speed: 800
 })
 
+// const apiURl = "https://api.weatherapi.com/v1/current.json?q=Cancun&key=4e5e576e864142e780a45600252403"
+
+//   fetch(apiURl)
+//     .then(response => {
+//     if(!response.ok) {
+//       throw new Error("Network response was not OK");
+//     }
+//     return response.json();
+//     })
+//     .then(data => {
+//     console.log(data);
+//     var weatherInfo = data;
+//     document.getElementById("location").innerText = `Location : ${weatherInfo.location.name} - ${weatherInfo.location.country}`;
+//     condition.src = "https:" + weatherInfo.current.condition.icon
+//     temp.innerHTML = "Temperature: " + weatherInfo. current.temp_c + "°C";
+//     })
+//     .catch(error => {
+//     console.error("Error:", error);
+//     })
+
+
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav');
 
